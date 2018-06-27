8d54a420ae4181bd2119a7391ca925e9a611f395
MSLacerda <mathe_oare@hotmail.com>
2018-06-27 12:22:05 -0300

    Tv  added

--- src/containers/Tv.js
+++ src/containers/Tv.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-
+import { Grid, Container, Segment, Header } from 'semantic-ui-react';
+import "../App.css";
 
 class Tv extends Component {
+  constructor() {
+    super();
+
+  }
+
+
   render() {
     return (
-      <div>
-        <h2>TV</h2>
+      <div className="full-height">
+        <Grid verticalAlign='middle' columns={1} centered>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment>
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
